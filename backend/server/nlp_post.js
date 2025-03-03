9ba5afd0afc69061d358db9e045d5ddc7f72bc9a
Maanvi Shadakshari <maanvishadakshari21@gmail.com>
2025-03-03 17:25:51 +0530

    Updates

--- backend/server/nlp_post.js
+++ backend/server/nlp_post.js
@@ -3,15 +3,20 @@ const fs= require("fs");
 const path= require("path");
 const router= express.Router();
 const FILE_PATH= path.join(__dirname, "newdataset.csv");
-
+const JSON_PATH = path.join(__dirname, "data.json");
 // Post route
 
 router.post("/", async (req, res) => {
     try {
         const { text } = req.body;
+
         if (!text) {
             return res.status(400).json({ error: "No text provided" });
         }
+              // Create a JSON object
+              const jsonData = { text };
+              // Write JSON data to a file
+              fs.writeFileSync(JSON_PATH, JSON.stringify(jsonData, null, 2));
         const newsStudent = `"${text}"\n`;
    console.log(newsStudent);
     }
